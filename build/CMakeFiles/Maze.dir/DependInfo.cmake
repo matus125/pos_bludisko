016efc88f2c85fb664b1a22d19e0819d13353e56
DavidBarta <davidbarta0505@gmail.com>
2025-01-08 14:51:43 +0100

    main.c and CMakeLists added

---
+++ build/CMakeFiles/Maze.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/barta7/XD/game.c" "CMakeFiles/Maze.dir/game.c.o" "gcc" "CMakeFiles/Maze.dir/game.c.o.d"
+  "/home/barta7/XD/main.c" "CMakeFiles/Maze.dir/main.c.o" "gcc" "CMakeFiles/Maze.dir/main.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
